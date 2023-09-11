97c167a987c11a1b5d38f6c023cb94545e4e5677
daya-diaz <dayadiazprofissional@gmail.com>
2023-09-11 09:58:29 -0300

    criando Queue durante inicialização.

---
+++ src/main/java/me/dio/sacola/configuration/RabbitMQConfig.java
@@ -0,0 +1,28 @@
+package me.dio.sacola.configuration;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    
+    @Bean
+    public Queue queue() {
+        return new Queue("sacola.v1.pedidos-clientes");
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+     @Bean
+     public ApplicationListener<ApplicationReadyEvent> applicationReadyEventApplicationListener(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+     }
+}
